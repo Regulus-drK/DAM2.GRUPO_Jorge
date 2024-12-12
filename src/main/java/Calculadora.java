85fadb480623bfc6afc4422d814348931f09f2d0
alba <albag95591@gmail.com>
2024-12-12 12:12:15 +0100

    Update Calculadora.java
    añade el porcentaje

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -27,4 +27,13 @@ public class Calculadora {
         }
         return Math.sqrt(numero);
     }
+
+    
+ public static double porcentaje(double num1, double num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("No se puede calcular el porcentaje con divisor cero.");
+        }
+            double resultado = (num1 * num2) / 100.0;
+        return resultado;
+    }
 }
